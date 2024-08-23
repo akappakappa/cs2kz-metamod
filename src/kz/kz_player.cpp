59ac22de3a5edfda01b483c984fb1bd6b769c716
GameChaos <franzkits@gmail.com>
2024-08-23 16:13:04 +0300

    Stage zones work

--- src/kz/kz_player.cpp
+++ src/kz/kz_player.cpp
@@ -969,7 +969,7 @@ void KZPlayer::MappingApiTriggerStartTouch(const KzTrigger *touched, const KzCou
 
 		case KZTRIGGER_ZONE_STAGE:
 		{
-			// TODO:
+			this->timerService->StageZoneStartTouch(course, touched->zone.number);
 		}
 		break;
 
